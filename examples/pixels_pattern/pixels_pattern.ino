536d20aa25ff1e1834a4454be6c9ceca4fae37dd
Yihui Xiong <yihui_xiong@163.com>
2016-11-03 15:36:28 +0800

    pixels_pattern.ino add spectrum mode

--- examples/pixels_pattern/pixels_pattern.ino
+++ examples/pixels_pattern/pixels_pattern.ino
@@ -9,38 +9,40 @@
 #define PIXELS_SPACE    128
 
 Pixels *pixels;
-int pixels_state = 0;
-
-const char *pixels_patterns[] = {"sleep", "wakeup", "wait", "answer", "offline", "online"};
+volatile uint32_t color;
+volatile int pixels_state = 0;
 
 void touch_event(uint8_t id, uint8_t event) {
-    // id - touch sensor id (0 ~ 7), event - 1: touch, 0: release
+  // id - touch sensor id (0 ~ 7), event - 1: touch, 0: release
 }
 
 void spi_event(uint8_t addr, uint8_t *data, uint8_t len)
 {
   if (0 == addr) {
-    if (0 == data[0]) {
-      pixels->clear();
-      pixels->update();
-      pixels_state = -1;
-    } else if (1 == data[0]) {
-      for (int i = 0; i < PIXELS_NUM; i++) {
-        pixels->set_color(i, data[3], data[2], data[1]);
-      }
-      pixels->update();
-      pixels_state = -1;
-    } else if (2 == data[0]) {
-      for (int i = 0; i < PIXELS_NUM; i++) {
-        pixels->set_color(i, 0, 0xFF, 0);
-      }
-      pixels->update();
-      pixels_state = -1;
-    } else if (3 == data[0]) {
+    if (0 == data[0]) {   // all off
+      color = 0x000000;
+      pixels_state = 0;
+    } else if (1 == data[0]) {  // specific color
+      color = Pixels::RGB(data[3], data[2], data[1]);
+      pixels_state = 0;
+    } else if (2 == data[0] || 7 == data[0]) {    // listening mode, all green
+      color = Pixels::RGB(0, 0x40, 0);
+      pixels_state = 0;
+    } else if (3 == data[0]) {    // waiting mode
+      pixels_state = 1;
+    }  else if (4 == data[0]) {   // speaking mode
       pixels_state = 2;
-    }  else if (4 == data[0]) {
-      pixels_state = 3;
-    } 
+    }
+  } else if (0xA0 == addr) {       // spectrum data
+    pixels_state = -1;
+    for (int i = 0; i < PIXELS_NUM; i++) {
+      // Serial.print(data[i]);
+      // Serial.print(' ');
+      pixels->set_color(i, change_brightness(b2r(i * 255 / (PIXELS_NUM - 1)), data[i]));
+      // pixels->set_color(i, change_brightness(b2r(data[i]), data[i]));
+    }
+    pixels->update();
+    // Serial.print("\r\n");
   }
 }
 
@@ -48,34 +50,28 @@ void setup() {
   respeaker.begin();
 
   pixels = &respeaker.pixels();
-  respeaker.attach_touch_handler(touch_event);
+  //  respeaker.attach_touch_handler(touch_event);
   respeaker.attach_spi_handler(spi_event);
-  for (int i = 0; i < PIXELS_NUM; i++) {
-    pixels->set_color(i, 0, 0, 32);
+  for (int i = 0; i < 0x20; i++) {
+    pixels->set_color(Pixels::RGB(i, 0, 0));
+    pixels->update();
+    delay(100);
   }
-  pixels->update();
-
-  delay(1000);
-  pixels->clear();
-  pixels->update();
-
-  pixels_state = 3;
+  for (int i = 0; i < 0x20; i++) {
+    pixels->set_color(Pixels::RGB(0x1F - i, 0, 0));
+    pixels->update();
+    delay(100);
+  }
 }
 
 void loop() {
   static uint32_t last_time = 0;
 
   if (pixels_state == 0) {
-    pixels->clear();
+    pixels->set_color(color);
     pixels->update();
     pixels_state = -1;
   } else if (pixels_state == 1) {
-    for (int i = 0; i < PIXELS_NUM; i++) {
-      pixels->set_color(i, 0, 0xFF, 0);
-    }
-    pixels->update();
-    pixels_state = -1;
-  } else if (pixels_state == 2) {
     static uint32_t t = 0;
     for (int i = 0; i < PIXELS_NUM; i++) {
       pixels->set_color(i, triangular_color((t + i * PIXELS_SPACE) % (PIXELS_SPACE * PIXELS_NUM)));
@@ -86,7 +82,7 @@ void loop() {
     if (t >= (PIXELS_SPACE * PIXELS_NUM)) {
       t = 0;
     }
-  } else if (pixels_state == 3) {
+  } else if (pixels_state == 2) {
     static uint32_t arc[PIXELS_NUM] = {0x10, 0x20, 0x30, 0x50, 0x80, 0xC0, 0xFF, 0xC0, 0x80, 0x50, 0x30, 0x20};
     static uint8_t t = 0x80;
     static int8_t deta = 1;
@@ -108,34 +104,6 @@ void loop() {
         deta = -deta;
       }
     }
-  } else if (pixels_state == 4) {
-    for (int i = 0; i < PIXELS_NUM; i++) {
-      pixels->set_color(i, 0xFF, 0, 0);
-    }
-    pixels->update();
-    pixels_state = -1;
-  } else if (pixels_state == 5) {
-    static uint8_t on = 0;
-
-    if (!on) {
-      for (int i = 0; i < PIXELS_NUM; i++) {
-        pixels->set_color(i, 0, 0xFF, 0);
-      }
-      pixels->update();
-      on = 1;
-      last_time = millis();
-    } else {
-      uint32_t current = millis();
-      if ((uint32_t)(current - last_time) >= 1000) {
-        for (int i = 0; i < PIXELS_NUM; i++) {
-          pixels->set_color(i, 0, 0, 0);
-        }
-        pixels->update();
-
-        on = 0;
-        pixels_state = -1;
-      }
-    }
   }
 }
 
@@ -149,4 +117,37 @@ uint32_t triangular_color(uint32_t t) {
   }
 
   return c;
+}
+
+uint32_t b2r(uint8_t position) {
+  color_t color = {0};
+  if (position < 128) {
+    color.b = 255 - position * 2;
+    color.g = position * 2;
+  } else {
+    color.g = 255 - position * 2;
+    color.r = position * 2;
+  }
+
+  return color.c;
+}
+
+uint32_t change_brightness(uint32_t rgb, uint8_t level) {
+  color_t color;
+  color.c = rgb;
+  color.r = color.r * level / 255;
+  color.g = color.g * level / 255;
+  color.b = color.b * level / 255;
+
+  if (color.r < 4) {
+    color.r = 0;
+  }
+  if (color.g < 4) {
+    color.g = 0;
+  }
+  if (color.b < 4) {
+    color.b = 0;
+  }
+
+  return color.c;
 }
